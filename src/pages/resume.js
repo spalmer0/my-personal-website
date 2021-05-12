2f9e99dc7d2646ddd85908f9d9574f67797cf665
Stuart Palmer <palmersf@gmail.com>
2021-05-12 05:43:10 -0400

    updates resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const Resume = (props) => (
     <div>
         <h1>Resume</h1>
         <p>
-            My Resume
+            I am a Software engineer that 
+            has spent the past 15 years 
+            in IT project and product 
+            management.  Adept in a variety 
+            of programming languages, I 
+            have the ability to solve 
+            complex problems, and deliver 
+            thoughtful solutions to meet 
+            customer needs.
         </p>
+        <Link to="/portfolio">Some of my Projects</Link>
+        <h2>Skills</h2>
+        <h3>Programming Languages</h3>
+        <ul>
+            <li>CSS</li>
+            <li>HTML</li>
+            <li>JavaScript</li>
+            <li>jQuery</li>
+            <li>PostgreSQL</li>
+            <li>SQL</li>
+        </ul>
+        <h3>Frameworks</h3>
+        <ul>
+            <li>Django</li>
+            <li>Express</li>
+        </ul>
+
+
         
     </div>
 );
